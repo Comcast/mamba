01cc2678a46256398e2a10b198b1dd2f4ce99c2c
EdwardAntonio <edward.antonio@nbcuni.com>
2020-10-30 19:30:23 -0400

    Removed platforms limitation.

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "mamba",
-    platforms: [
-        .iOS(.v12), .tvOS(.v12)
-    ],
     products: [
         .library(
             name: "mamba",
