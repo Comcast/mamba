b3a8c3bf5a75b366b1acbb1de243a3d73e5baa9e
theRealRobG <robertnw5@gmail.com>
2024-09-03 22:49:26 -0400

    Updated media sequence calculation logic to avoid unnecessary looping
     Conflicts:
    	mambaSharedFramework/Playlist Models/Playlist Structure/PlaylistStructureCore.swift

--- mambaSharedFramework/Playlist Models/Playlist Structure/PlaylistStructureCore.swift
+++ mambaSharedFramework/Playlist Models/Playlist Structure/PlaylistStructureCore.swift
@@ -251,42 +251,42 @@ struct PlaylistStructureConstructor {
         var lastRecordedTime: CMTime = CMTime.invalid
         var currentSegmentDuration: CMTime = CMTime.invalid
         var discontinuity = false
-        
-        // collect indices for media sequence and skip tags as they impact the initial media sequence value
-        var mediaSequenceTagIndices = [Int]()
-        var skipTagIndices = [Int]()
-        tags.enumerated().forEach {
-            switch $0.element.tagDescriptor {
-            case PantosTag.EXT_X_MEDIA_SEQUENCE: mediaSequenceTagIndices.append($0.offset)
-            case PantosTag.EXT_X_SKIP: skipTagIndices.append($0.offset)
-            default: break
+
+        // collect media sequence and skip tag (if they exist) as they impact the initial media sequence value
+        var mediaSequenceTag: PlaylistTag?
+        var skipTag: PlaylistTag?
+        for tag in tags {
+            switch tag.tagDescriptor {
+            case PantosTag.EXT_X_MEDIA_SEQUENCE: mediaSequenceTag = tag
+            case PantosTag.EXT_X_SKIP: skipTag = tag
+            case PantosTag.Location:
+                // Both the EXT-X-MEDIA-SEQUNCE and the EXT-X-SKIP tag are expected to occur before any Media Segments.
+                //
+                // For EXT-X-MEDIA-SEQUNCE section 4.4.3.2 indicates:
+                //     The EXT-X-MEDIA-SEQUENCE tag MUST appear before the first Media Segment in the Playlist.
+                //
+                // For EXT-X-SKIP section 4.4.5.2 indicates:
+                //     A server produces a Playlist Delta Update (Section 6.2.5.1), by replacing tags earlier than the
+                //     Skip Boundary with an EXT-X-SKIP tag. When replacing Media Segments, the EXT-X-SKIP tag replaces
+                //     the segment URI lines and all Media Segment Tags tags that are applied to those segments.
+                //
+                // Exiting early at the first Location helps us avoid having to loop through the entire playlist when we
+                // know that the tags we're looking for MUST NOT exist.
+                break
+            default: continue
             }
         }
 
-        // figure out our media sequence start (defaults to 1 if not specified)
-        if mediaSequenceTagIndices.count > 0 {
-            assert(mediaSequenceTagIndices.count == 1, "Unexpected to have more than one media sequence")
-            if
-                let mediaSequenceIndex = mediaSequenceTagIndices.first,
-                let startMediaSequence: MediaSequence = tags[mediaSequenceIndex].value(
-                    forValueIdentifier: PantosValue.sequence
-                )
-            {
-                currentMediaSequence = startMediaSequence
-            }
+        // figure out our media sequence start (defaults to 0 if not specified)
+        if let startMediaSequence: MediaSequence = mediaSequenceTag?.value(forValueIdentifier: PantosValue.sequence) {
+            currentMediaSequence = startMediaSequence
         }
 
-        // account for any skip tags (since a delta update replaces all segments earlier than the skip boundary, the
+        // account for any skip tag (since a delta update replaces all segments earlier than the skip boundary, the
         // SKIPPED-SEGMENTS value will effectively update the current media sequence value of the first segment, so safe
         // to do this here and not within the looping through media group tags below).
-        if skipTagIndices.count > 0 {
-            assert(skipTagIndices.count == 1, "Unexpected to have more than one skip tag")
-            if
-                let skipTagIndex = skipTagIndices.first,
-                let skippedSegments: Int = tags[skipTagIndex].value(forValueIdentifier: PantosValue.skippedSegments)
-            {
-                currentMediaSequence += skippedSegments
-            }
+        if let skippedSegments: Int = skipTag?.value(forValueIdentifier: PantosValue.skippedSegments) {
+            currentMediaSequence += skippedSegments
         }
         
         // find the "header" portion by finding the first ".mediaSegment" scoped tag
